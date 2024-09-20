c828140a4b935305daed7fccd600da9d22b28a2d
Ever-Daniel-Pereyra-Castillo <a.22308051281282@cbtis128.edu.mx>
2024-09-20 09:34:52 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,49 @@
+print("Act 9 clases v3")
+print("Ever Pereyra Mat: 22308051281282")
+# zona de class
+class Operadores1282:
+    # Zona de funciones
+    def suma1282(self,E,P):
+        s1282=E+P
+        print(f"La suma de {E} + {P} = {s1282}")
+    def resta1282(self,A,B):
+        r1282= A-B
+        print(f"La resta de {A} - {B} = {r1282}")    
+    def multiplicacion1282(self,C,D):
+        m1282=C*D
+        print(f"La Multiplicacion de {C} * {D} = {m1282}")
+    def division1282(self,F,G):
+        d1282=F/G
+        print(f"La division de {F} / {G} = {d1282}")
+    def modulo1282(self,H,I):
+        mo1282=H%I
+        print(f"El modulo de {H} % {I} = {mo1282}")
+    def exponente1282(self,J,K):
+        e1282=J**K
+        print(f"El exponente de {J} ** {K} = {e1282}")
+    def cociente1282(self,L,M):
+        c1282=L//M
+        print(f"El cociente de {L} // {M} = {c1282}")
+
+# creacion del objeto
+operapereyra=Operadores1282()
+
+# zona de uso de objetos
+print("Función Suma")
+operapereyra.suma1282(3,4)
+print("Función Resta")
+operapereyra.resta1282(5,2)
+print("Función Multiplicacion")
+operapereyra.multiplicacion1282(5,2)
+print("Función Division")
+operapereyra.division1282(8,2)
+print("Función Modulo")
+operapereyra.modulo1282(3,2)
+print("Función Exponente")
+operapereyra.exponente1282(4,3)
+print("Función Cociente")
+operapereyra.cociente1282(9,3)
+
+
+
+
